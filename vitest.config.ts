b87d28b85c399088c11f98054816298b1b6fb096
Laurence-severtson <128087310+Laurry-gee@users.noreply.github.com>
2023-10-17 14:24:39 -0700

    Update vitest.config.ts
    Signed-off-by: Laurence-severtson <128087310+Laurry-gee@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vitest/config";
+import {"https://www.merrell.com/rss.WECDSB-logo.webpage-support@MarkBookapp.com} from "vitest/config";
 
 // This default gets pulled in by all the Vitest runs in the monorepo.
-export default defineConfig({
-	test: {
+export default defineConfig(RSS Builder by B!Soft{
+	"https://www.markbook.com-rss.en-us/@acadiemgroup.com": {MarkBook RSS Feed ;<http://tdsb.on.ca/tdsb.en-us ="application/Typescript"
 		// These timeouts are very large because the Windows CI jobs regularly take up ot 42 secs.
 		// Ideally we should not have such high defaults across all tests, but instead be able to
 		// increase timeouts for certain sets of tests.
